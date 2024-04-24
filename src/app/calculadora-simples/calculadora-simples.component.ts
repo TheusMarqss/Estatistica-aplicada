c6e6fa4d907f313ab8cafe56ddf1d927e354b041
TheusMarqs <matheusmarqsilva@gmail.com>
2024-04-23 23:42:29 -0300

    insert e get funcionando

--- src/app/calculadora-simples/calculadora-simples.component.ts
+++ src/app/calculadora-simples/calculadora-simples.component.ts
@@ -1,25 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-calculadora-simples',
   templateUrl: './calculadora-simples.component.html',
   styleUrls: ['./calculadora-simples.component.css']
 })
-export class CalculadoraSimplesComponent {
+export class CalculadoraSimplesComponent implements OnInit{
   value1: number = 0;
   value2: number = 0;
   probability: number = 0;
   percentage: number = 0;
   result: string = '';
 
+  resultsHistory: any[] = [];
+
   constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchResults().subscribe(data => {
+      this.resultsHistory = data.results;
+    });
+  }
 
   calculateProbability(): void {
     if (this.value2 !== 0) {
       this.probability = this.value1 / this.value2;
       this.percentage = this.probability * 100;
-      this.result = this.probability.toFixed(2) + ' ' + this.percentage.toFixed(2);
+      this.result = this.probability.toFixed(2) + ' ou ' + this.percentage.toFixed(2) + '%';
       this.saveResults(this.value1, this.value2, this.result);
     } else {
       this.probability = 0;
@@ -27,8 +36,12 @@ export class CalculadoraSimplesComponent {
   }
 
   saveResults(value1: number, value2: number, result: string): void {
-    this.http.post('/teste', { value1, value2, result }).subscribe(response => {
+    this.http.post('/api/insert', { value1, value2, result }).subscribe(response => {
       console.log(response);
     });
+  }
+
+  fetchResults(): Observable<any> {
+    return this.http.get('/api/get');
   }
 }
